a11125b41083fb790c887b8316c0cbac697249fe
Ethan Smith <ethan@ethanhs.me>
2020-01-22 01:26:19 -0800

    Use stdout encoding when decoding

--- abserde/gen_crate.py
+++ abserde/gen_crate.py
@@ -30,7 +30,7 @@ def generate_crate(mod: str, config: Config) -> None:
         cmd.append("--release")
         env['RUSTFLAGS'] = "-C target-cpu=native"
     p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=crate_dir, env=env)
-    print(p.stdout.decode(), p.stderr.decode())
+    print(p.stdout.decode(sys.stdout.encoding), p.stderr.decode(sys.stdout.encoding))
     wheelhouse = crate_dir / "target" / "wheels"
     if config.debug:
         print("Generated wheel")
